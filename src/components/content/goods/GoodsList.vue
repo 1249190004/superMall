b6b34efae232bb93f7ae0f2f896b4d3b1d243259
1249190004 <1249190004@qq.com>
2020-08-16 09:36:33 +0800

    msg

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="goods">
+    
+  </div>
+</template>
+
+<script>
+  import GoodsListItem from "./GoodsListItem";
+  export default {
+    name: "GoodsList",
+    props: {
+      goods: {
+        type: Object,
+        default() {
+          return []
+        }
+      }
+    },
+    components:{
+      GoodsListItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img :src="goodsItem.show.img" alt="">
+    <div>
+      <p>{{goodsItem.title}}</p>
+      <span class="price">{{goodsItem.price}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GoodsListItem",
+    props: {
+      goodsItem: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
